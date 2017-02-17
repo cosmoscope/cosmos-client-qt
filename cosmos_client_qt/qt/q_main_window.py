348f6f77fd3bcac324731bb66ed3595b83496fac
nmearl <nchlsearl@gmail.com>
2017-02-16 19:32:12 -0500

    Safety

---
+++ cosmos_client_qt/qt/q_main_window.py
@@ -0,0 +1,10 @@
+from pyqt.QtWidgets import QMainWindow
+
+
+class MainWindow(QMainWindow):
+    """
+    Main application window.
+    """
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from cosmos_client_qt.client import run
+import sys
+from qtpy.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow
+from cosmos_client_qt.ui.widgets.docking.q_dock_area import QDockArea
+from cosmos_client_qt.ui.widgets.docking.q_dock_item import QDockItem
 
-
-def main():
-    run()
-
+from qtpy.uic import loadUi
+import os
 
 if __name__ == '__main__':
-    main()
+
+    app = QApplication(sys.argv)
+
+    w = loadUi(os.path.join(os.path.dirname(__file__), 'cosmos_client_qt', 'ui', 'mainwindow.ui'))
+    d = loadUi(os.path. join(os.path.dirname(__file__), 'cosmos_client_qt', 'ui', 'data_item.ui'))
+
+    w.verticalLayout_2.addWidget(d)
+
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
