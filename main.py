2040010a92d90100b624ecd9c94ddd536e8fa7db
nmearl <contact@nicholasearl.me>
2017-02-21 09:20:22 -0500

    Define viewer main window

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
-from qtpy.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow
-from cosmos_client_qt.qt.main_window import MainWindow
-
-from qtpy.uic import loadUi
 import os
+
+from qtpy.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow
+from qtpy.uic import loadUi
+
+from cosmos_client_qt.ui import resource
+
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
 
-    w = MainWindow()
+    w = loadUi(os.path.join(os.path.dirname(__file__), "cosmos_client_qt", "ui", "cosmoscope-qt.ui"))
 
     w.setWindowTitle('Simple')
     w.show()
